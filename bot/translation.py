6447a0aff6d3ee203b3d17d8450a5100fa9db364
Moringtar2 <91413451+Moringtar2@users.noreply.github.com>
2021-09-27 13:03:43 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,21 +4,21 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+    START_TEXT = """<b>😇Hey {}!!</b>
+<i>Am Just A Advance Auto Filter Bot V2.1....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+😜Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@ONLY_CODES
 </i>"""    
     
     HELP_TEXT = """
 <b><i><u>How To Use Me!?</u></i></b>
 
 <i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
+-> Add Me To Any Group And Make Me Admin🥳
+-> Add Me To Your Desired Channel😜
 </i>
 
 <b>Bot Commands (Works Only In Groups) :</b>
@@ -43,16 +43,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@AD_BOTZ
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> 🌀Auto Filter Bot V2.1🌀</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ 🧑‍✈️Creator</b> : <b><i><a href="https://t.me/Lucifer_Devil_AD">Lucifer Moring star</a></i></b>
 
-<b>➥ Language</b> : <code>Python3</code>
+<b>➥ 📜Language</b> : <code>Python3</code>
 
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+<b>➥ 📝Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ 🗒️Source Code</b> : <i><a href="https://github.com/Moringtar2/auto-filter-botV2.1">Click Me</a></i>
 """
